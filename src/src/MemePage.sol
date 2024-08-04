942ba8dab912d775914160101b0b82b9d5492b8d
jindra12.underdark <jindra12.underdark@gmail.com>
2024-08-04 23:37:43 +0200

    BE contract basic compiles

--- src/src/MemePage.sol
+++ src/src/MemePage.sol
@@ -1,9 +1,12 @@
-// SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.4.16 <0.9.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
 import {Ownable} from "../lib/openzeppelin-contracts/contracts/access/Ownable.sol";
 import {ReverseRegistrar} from "../lib/ens-contracts/contracts/reverseRegistrar/ReverseRegistrar.sol";
 import {ENS} from "../lib/ens-contracts/contracts/registry/ENS.sol";
-
+import { MarketAPI } from "filecoin-solidity-api/contracts/v0.8/MarketAPI.sol";
+import { CommonTypes } from "filecoin-solidity-api/contracts/v0.8/types/CommonTypes.sol";
+import { MarketTypes } from "filecoin-solidity-api/contracts/v0.8/types/MarketTypes.sol";
+import { BigIntCBOR } from "filecoin-solidity-api/contracts/v0.8/cbor/BigIntCbor.sol";
 
 contract MemePage is Ownable {
     constructor(ENS ens, string memory name, bytes32 addressReverseNode) {
