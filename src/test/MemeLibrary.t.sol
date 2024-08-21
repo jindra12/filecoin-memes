a41fe9768275997d5b89370a61310a4c92a10940
jindra12.underdark <jindra12.underdark@gmail.com>
2024-08-22 01:53:34 +0200

    Start developing unit tests

---
+++ src/test/MemeLibrary.t.sol
@@ -0,0 +1,59 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+import {Test, console} from "forge-std/Test.sol";
+import {MemeStructs} from "../src/MemeStructs.sol";
+
+contract MemeLibraryTest is Test {
+    function testGetDay() public pure {
+
+    }
+    function testGetWeek() public pure {
+
+    }
+    function testGetMonth() public pure {
+
+    }
+    function testComparePostsByTime() public pure {
+
+    }
+    function testComparePostsByHot() public pure {
+
+    }
+    function testComparePosts() public pure {
+
+    }
+    function testAddSortedPosts() public pure {
+
+    }
+    function testSlicePosts() public pure {
+
+    }
+    function testMergeSortPosts() public pure {
+
+    }
+    function testAddSortedComments() public pure {
+
+    }
+    function testSliceComments() public pure {
+        
+    }
+    function testMergeSortComments() public pure {
+
+    }
+    function testCompareCommentsByTime() public pure {
+
+    }
+    function testCompareCommentsByHot() public pure {
+
+    }
+    function testCompareCommentsByLike() public pure {
+
+    }
+    function testCompareComments() public pure {
+
+    }
+    function testFilterPostByAuthor() public pure {
+        
+    }
+}
