f73dce178ace9e6f385467d86a2f0f92049a6ac3
jindra12.underdark <jindra12.underdark@gmail.com>
2024-08-24 21:14:36 +0200

    Finish basic library test

--- src/test/MemeLibrary.t.sol
+++ src/test/MemeLibrary.t.sol
@@ -80,30 +80,72 @@ contract MemeLibraryTest is Test {
         assertEq(slice2.length, 3);
     }
     function testMergeSortPosts() public {
-
-    }
-    function testAddSortedComments() public {
-
-    }
-    function testSliceComments() public {
-        
-    }
-    function testMergeSortComments() public {
-
+        TestLibrary.populatePosts(vm, posts, 11);
+        MemeStructs.Post[] memory sorted = MemeLibrary.mergeSortPosts(posts, MemeStructs.SortType.TIME);
+        assertEq(sorted.length, 11);
+        for (uint256 i = 1; i < sorted.length; i++) {
+            assertTrue(sorted[i - 1].time < sorted[i].time);
+        }
     }
     function testCompareCommentsByTime() public {
-
+        TestLibrary.populateComments(vm, comments, 2);
+        assertTrue(MemeLibrary.compareCommentsByTime(comments[0], comments[1]));
+        assertFalse(MemeLibrary.compareCommentsByTime(comments[1], comments[0]));
     }
     function testCompareCommentsByHot() public {
-
+        TestLibrary.populateComments(vm, comments, 2);
+        assertTrue(MemeLibrary.compareCommentsByHot(comments[1], comments[0]));
+        assertFalse(MemeLibrary.compareCommentsByHot(comments[0], comments[1]));
     }
     function testCompareCommentsByLike() public {
-
+        TestLibrary.populateComments(vm, comments, 2);
+        assertTrue(MemeLibrary.compareCommentsByLike(comments[1], comments[0]));
+        assertFalse(MemeLibrary.compareCommentsByLike(comments[0], comments[1]));
     }
     function testCompareComments() public {
-
+        TestLibrary.populateComments(vm, comments, 2);
+        assertTrue(MemeLibrary.compareComments(comments[0], comments[1], MemeStructs.SortType.TIME));
+        assertFalse(MemeLibrary.compareComments(comments[1], comments[0], MemeStructs.SortType.TIME));
+        assertTrue(MemeLibrary.compareComments(comments[1], comments[0], MemeStructs.SortType.HOT));
+        assertFalse(MemeLibrary.compareComments(comments[0], comments[1], MemeStructs.SortType.HOT));
+        assertTrue(MemeLibrary.compareComments(comments[1], comments[0], MemeStructs.SortType.LIKE));
+        assertFalse(MemeLibrary.compareComments(comments[0], comments[1], MemeStructs.SortType.LIKE));
+    }
+    function testAddSortedComments() public pure {
+        MemeStructs.Comment[] memory aS = new MemeStructs.Comment[](3);
+        MemeStructs.Comment[] memory bS = new MemeStructs.Comment[](3);
+        for (uint256 i = 0; i < aS.length; i++) {
+            aS[i].time = i;
+            bS[i].time = i;
+        }
+        MemeStructs.Comment[] memory added = MemeLibrary.addSortedComments(aS, bS, MemeStructs.SortType.TIME);
+        assertEq(added[0].time, 0);
+        assertEq(added[1].time, 0);
+        assertEq(added[2].time, 1);
+        assertEq(added[3].time, 1);
+        assertEq(added[4].time, 2);
+        assertEq(added[5].time, 2);
+    }
+    function testSliceComments() public pure {
+        MemeStructs.Comment[] memory aS = new MemeStructs.Comment[](6);
+        MemeStructs.Comment[] memory slice1 = MemeLibrary.sliceComments(aS, 0, 3);
+        assertEq(slice1.length, 3);
+        MemeStructs.Comment[] memory slice2 = MemeLibrary.sliceComments(aS, 3, 6);
+        assertEq(slice2.length, 3);
+    }
+    function testMergeSortComments() public {
+        TestLibrary.populateComments(vm, comments, 11);
+        MemeStructs.Comment[] memory sorted = MemeLibrary.mergeSortComments(comments, MemeStructs.SortType.TIME);
+        assertEq(sorted.length, 11);
+        for (uint256 i = 1; i < sorted.length; i++) {
+            assertTrue(sorted[i - 1].time < sorted[i].time);
+        }
     }
     function testFilterPostByAuthor() public {
-
+        TestLibrary.populatePosts(vm, posts, 11);
+        address someAuthor = posts[5].author;
+        assertTrue(MemeLibrary.filterPostByAuthor(posts[5], address(0)));
+        assertTrue(MemeLibrary.filterPostByAuthor(posts[5], someAuthor));
+        assertFalse(MemeLibrary.filterPostByAuthor(posts[4], someAuthor));
     }
 }
